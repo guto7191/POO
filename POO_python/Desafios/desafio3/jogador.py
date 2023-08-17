216a55028966958c8c007874e3aa9dc862dd4b1e
Augusto <luzguga2010@gmail.com>
2023-08-17 09:36:17 -0300

    Adcionando algumas funções

--- POO_python/Desafios/desafio3/jogador.py
+++ POO_python/Desafios/desafio3/jogador.py
@@ -3,3 +3,7 @@ class Jogador:
 
     def __init__(self, nome: str, jogada: int) -> None:
         pass
+    
+
+    def marcar_posicao(self):
+        
--- POO_python/Desafios/desafio3/jogo_da_velha.py
+++ POO_python/Desafios/desafio3/jogo_da_velha.py
@@ -5,4 +5,7 @@ class JogoDaVelha:
 
     def __init__(self, jogador_1: Type[Jogador], jogador_2: Type[Jogador]) -> None:
         self.tabuleiro = Tabuleiro()
+        self.jogador_1 = jogador_1
+        self.jogador_2 = jogador_2
+    
     
