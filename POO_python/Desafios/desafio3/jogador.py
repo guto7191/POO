eddc3f4754588eeb524dfb4f323ac9cabfe2f3d4
Augusto <luzguga2010@gmail.com>
2023-08-18 20:01:23 -0300

    Validação melhorada e print o tabuleiro

--- POO_python/Desafios/desafio3/jogador.py
+++ POO_python/Desafios/desafio3/jogador.py
@@ -7,15 +7,20 @@ class Jogador:
     def marcar_posicao(self):
         var = int(input(f"Escolha a sua posição {self.get_nome()}: "))
         self.validar_posicao(var)
+    
+
+    def repitir_marcar_posicao(self):
+        var = int(input(f"Valor inválido escolha a sua posição {self.get_nome()}: "))
+        while var <= 0 and var < 9:
+            var = int(input(f"Valor inválido escolha a sua posição {self.get_nome()}: "))
 
 
-    def validar_posicao(self, valor: int) -> bool:
+    def validar_posicao(self, valor: int):
         # melhorar isso depois
-        if valor <= 0 and valor > 9:
+        if valor <= 0 and valor < 9:
             print("Valor Inválido!")
-            return False 
-        else:
-            return True
+            self.repitir_marcar_posicao()
+
 
     def get_nome(self):
         return self.nome
--- POO_python/Desafios/desafio3/tabuleiro.py
+++ POO_python/Desafios/desafio3/tabuleiro.py
@@ -1,8 +1,18 @@
 class Tabuleiro:
 
     def __init__(self):
-        pass
-    
+        self.matrix = [[0,0,0],[0,0,0],[0,0,0]]
+
 
     def print_tabuleiro(self):
-        print("Tabuleiro Na Tela")
+        print("Tabuleiro!\n ")
+        for i in range(len(self.matrix)):
+            for j in range(len(self.matrix[i])):
+                if (j != 2):
+                    print(f"{self.matrix[i][j]} |", end=" ")      
+                else:
+                    print(self.matrix[i][j], end=" ")
+            if(i!=2):
+                print('\n' + '_'* 10 + '\n')
+            else: 
+                print("\n")
